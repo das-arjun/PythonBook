709476983026e68347b31ac7a2d0bd75e86a4b53
Arjun Das <arjun.suman.das@gmail.com>
2026-03-18 12:19:21 +0530

    Update 3_4_5.py

--- 3_4_5.py
+++ 3_4_5.py
@@ -1,4 +1,11 @@
+import turtle as t
+wn = turtle.Screen()
+wn.bgcolor("lightblue") # Optional: set background color
+t = t.Turtle()
 i = 0
 while 3 = i
 i += 1
 print(i)
+t.forward(100)
+t.right(90)
+t.forward(100)
