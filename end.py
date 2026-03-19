a557efc7a585e218bdc1c87d0258cf892281295c
Arjun Das <arjun.suman.das@gmail.com>
2026-03-19 11:54:32 +0530

    Update and rename aa to end.py

---
+++ end.py
@@ -0,0 +1,3 @@
+green = '\033[32m'   # ANSI code for green. (not part of book)
+def congrats(name):    # The final chapter.
+  print(f"{green}Congrats {name}, you just completed the book of Python programming for kids!")
